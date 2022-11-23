2baf0fb707c4be34a2fbc0020d2fa09f7a4fc0bc
Florian Van Belleghem <fvbe@stylelabs.com>
2022-11-23 21:36:31 +0100

    Create oscillator controller

---
+++ src/components/OscillatorController.tsx
@@ -0,0 +1,33 @@
+import { FunctionComponent, useEffect, useState } from "react";
+import FrequencyVisualizer from "./FrequencyVisualizer";
+import LogarithmicRange from "./LogarithmicRange";
+
+interface OscillatorControllerProps {
+  context: AudioContext;
+  oscillator: OscillatorNode;
+}
+
+const OscillatorController: FunctionComponent<OscillatorControllerProps> = ({
+  context,
+  oscillator,
+}) => {
+  const [hertz, setHertz] = useState(440);
+
+  useEffect(() => {
+    oscillator.frequency.setValueAtTime(hertz, context.currentTime);
+  }, [oscillator, hertz]);
+
+  return (
+    <>
+      <LogarithmicRange
+        minimumValue={20}
+        maximumValue={20000}
+        defaultValue={hertz}
+        onChange={setHertz}
+      />
+      <FrequencyVisualizer context={context} input={oscillator} />
+    </>
+  );
+};
+
+export default OscillatorController;

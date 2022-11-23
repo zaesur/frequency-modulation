850d9f91f8a77042e360ba5261d2d97fb353b04c
Florian Van Belleghem <fvbe@stylelabs.com>
2022-11-23 21:37:52 +0100

    Create FM sandbox

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,31 @@
-import { FunctionComponent } from "react";
+import { FunctionComponent, useEffect, useRef } from "react";
+import OscillatorController from "./OscillatorController";
 
 const App: FunctionComponent = () => {
-  return null;
+  const { current: context } = useRef(new AudioContext());
+  const { current: oscillatorNodeOne } = useRef(new OscillatorNode(context));
+  const { current: oscillatorNodeTwo } = useRef(new OscillatorNode(context));
+  const { current: gainNode } = useRef(new GainNode(context));
+
+  useEffect(() => {
+    oscillatorNodeOne.connect(gainNode).connect(context.destination);
+    oscillatorNodeTwo.connect(gainNode).connect(context.destination);
+  }, []);
+
+  return (
+    <>
+      <button
+        onClick={() => {
+          oscillatorNodeOne.start(context.currentTime);
+          oscillatorNodeTwo.start(context.currentTime);
+        }}
+      >
+        Play
+      </button>
+      <OscillatorController context={context} oscillator={oscillatorNodeOne} />
+      <OscillatorController context={context} oscillator={oscillatorNodeTwo} />
+    </>
+  );
 };
 
 export default App;
